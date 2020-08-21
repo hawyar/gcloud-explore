31a49c46657fe87dc08afa46d2f6f9e7e4a8ef38
Hawyar <hawyarxoshnaw@gmail.com>
2020-08-20 23:33:57 -0400

    add:  spicy mkdir 🔥

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,36 +1,50 @@
 module.exports = {
   /**
-   * Create base directory
-   * @param dir String, Directory to save the JSON file
-   * @param recursive Boolean, defaults to false
-   * @param callback
+   * @param {String} directory
    */
+  mkdirSync(directory) {
+    const paths = directory.replace(/\/$/, '').split('/');
 
-  makeDir(dir, recursive = false, callback) {
-    fs.mkdirSync(dir, { recursive }, (err) => {
-      if (err) return;
-      console.log(`✅ Success: created ${dir}`);
+    paths.forEach((_, index, dir) => {
+      index += 1;
+      const path = dir.slice(0, index).join('/');
+
+      !fs.existsSync(path) ? fs.mkdirSync(path) : null;
     });
-    callback();
   },
 
-  /**
-   * Create JSON file
-   * @param dir String, Directory to save the JSON file
-   * @param data Object, data to be parsed
-   * @param file String, name of the file
-   * @param callback
-   */
+  //   /**
+  //    * Create base directory
+  //    * @param dir String, Directory to save the JSON file
+  //    * @param recursive Boolean, defaults to false
+  //    * @param callback
+  //    */
 
-  makeJson(dir, data = {}, file, callback) {
-    if (!fs.existsSync(dir)) {
-      makeDir(dir, true, () => {
-        console.log(`${dir} newly created`);
-      });
-    }
-    fs.writeFile(path.join(dir, file), JSON.stringify(data, null, 4), (err) => {
-      if (err) return err;
-    });
-    callback();
-  },
+  //   makeDir(dir, recursive = false, callback) {
+  //     fs.mkdirSync(dir, { recursive }, (err) => {
+  //       if (err) return;
+  //       console.log(`✅ Success: created ${dir}`);
+  //     });
+  //     callback();
+  //   },
+
+  //   /**
+  //    * Create JSON file
+  //    * @param dir String, Directory to save the JSON file
+  //    * @param data Object, data to be parsed
+  //    * @param file String, name of the file
+  //    * @param callback
+  //    */
+
+  //   makeJson(dir, data = {}, file, callback) {
+  //     if (!fs.existsSync(dir)) {
+  //       makeDir(dir, true, () => {
+  //         console.log(`${dir} newly created`);
+  //       });
+  //     }
+  //     fs.writeFile(path.join(dir, file), JSON.stringify(data, null, 4), (err) => {
+  //       if (err) return err;
+  //     });
+  //     callback();
+  //   },
 };
